394a40467f3420fb9483d06209ccf39628974e9c
Dimitris Voudouris <dvoudouris@fb06mac-u101473.allpsych.uni-giessen.de>
2024-10-28 20:35:54 +0100

    Initial commit of MATLAB intro course material

---
+++ MATLAB_Intro/01_basics/01_basics_exc.m
@@ -0,0 +1,85 @@
+%%% Introduction to programming with MATLAB
+%%% Topic #1 - Introductory operations
+%%% Objectives: Familiarize with MATLAB environment - Operations with scalars and vectors
+%%% 
+%%% Dimitris Voudouris, October 2024
+%%% Practice
+
+
+%% Exercise 1
+% a. Multiply 3 by 8 and divide this product by the difference between 13 and 7
+
+% b. If you sleep on average 6h per night, how many seconds did you spend sleeping the last two nights?
+
+
+%% Exercise 2
+% a. Calculate the number of seconds in a non-leap year (365 days)
+% Assign the answer to a variable with a meaningful name.
+
+% b. Create a column vector named "threes" that contains 795 times the value 3
+
+% c. Create a vector named "desc" with values starting from 100 and ending at 0 in steps of 2 (100, 98, 96, ..., 0)
+
+
+%% Exercise 3
+% a. There is a right triangle with sides a = 15, b = 20, c = ?
+% Find the length of side c using the Pythagorean theorem (look it up if you do not remember it)
+% The value of c is equal to the square root of c to the power of 2. 
+
+% b. How would you calculate c, if a = 19 and b = 23?
+
+
+%% Exercise 4
+% There is a person who likes brewing beer. They store the beer in a
+% cylindrical barrel that has a length of 2 m and a radius of 2 m. Now
+% they want to construct another barrel with the same length (2 m) but with volume that is 20% more
+% than that of the current barrel. How large should the radius of the new barrel be?
+% Note: The volume of the barrel is equal to V = ? * r^2 * h, and that pi = 3.14
+% (r is the radius, h is the height)
+
+
+%% Exercise 5
+% Create an m-file with a script that converts a Fahrenheit temperature (say, 95 deg) to Celsius. 
+% Think of a reasonable name when saving this m-file. 
+% In how many ways can you execute this file? Try them out.
+% To do the conversion from F to C, subtract 32 from the Fahrenheit value and multiply the difference by 5/9.
+
+
+%% Exercise 6
+% Create a vector "age_t1" with the ages of a football team's players:                  [22, 21, 25, 19, 30, 28, 31, 36, 20, 23, 22]
+% Create a new vector "age_t1_future" that contains the ages of these people in 3 years
+% Create another vector "age_t2" with the ages of another team's players:               [25, 23, 25, 26, 29, 33, 32, 21, 27, 30, 30]
+% Calculate variable "age_diff" as the difference in age between "age_t2" and "age_t1"
+% What is the average value of the new vector "age_diff"?
+
+
+%% Exercise 7
+% a. Create the following vectors:
+% Vector x contains numbers from 1 to 100 in steps of 1
+% Vector y contains numbers from 10 to 0 in steps of 0.1
+% Vector z contains the first 50 elements of x, and the last 50 elements of y
+
+% b. Create vector v1 that contains 78 sevens: (7 7 7...7)
+% Set the value of the 2nd and every other element after that to 99:
+% so vector v1 should be: 7 99 7 99 ... 99
+
+% c. Create new vector v2 with values between 10 and 199 in steps of 3
+% Create vector v3 that contains the last 39 elements of v2
+
+% d. Assign the 39 values of v3 to the 39 indices of v1 that have values equal to 99
+
+
+%% Exercise 8
+% a. Create a set of column vectors related to a dummy experiment:
+% The first vector is named 'subject' and contains the subject ID (1 to 15).
+% The next is named 'age' and has the age of each subject. 
+%   Create the age vector by using random integers between 18 and 35 (you need to look up how to do so).
+% The third vector is named 'height' and has the height (in cm) of each subject. 
+%   Create such a vector with random integers between 155 and 190.
+
+% b. Calculate the median age and the median height across your subjects
+% Can you find two different ways to calculate the medians?
+    
+    
+%% END OF EXERCISES
+
